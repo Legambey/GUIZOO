9c97073db87cdc39b74962e9473e224cd0de3b4c
Wakuir <113540865+Wakuir@users.noreply.github.com>
2023-12-20 16:40:03 +0100

    Added delete buttons

--- src/main/java/fr/nsi/content/ManagePage.java
+++ src/main/java/fr/nsi/content/ManagePage.java
@@ -7,11 +7,14 @@ import fr.nsi.util.RequestResponse;
 import fr.nsi.util.RowData;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ManagePage extends ContentPanel {
     TableView<RowData> tableView;
@@ -26,10 +29,25 @@ public class ManagePage extends ContentPanel {
     @Override
     public void init(PanelManager panelManager) {
         super.init(panelManager);
-
         GridPane centerPane = new GridPane();
         this.layout.add(centerPane, 0, 0);
         setCanTakeAllSize(centerPane);
+
+        ScrollPane scrollPane = new ScrollPane();
+        setCanTakeAllSize(scrollPane);
+
+        VBox vBox = new VBox();
+        setCanTakeAllSize(vBox);
+        vBox.minWidthProperty().bind(scrollPane.widthProperty());
+        vBox.minHeightProperty().bind(scrollPane.heightProperty());
+        addContent(vBox);
+
+        centerPane.getChildren().addAll(scrollPane);
+        scrollPane.setContent(vBox);
+    }
+
+    void addContent(VBox vbox){
+        GridPane gridPane = new GridPane();
 
         ComboBox<String> tableSelector = new ComboBox<>();
 
@@ -45,18 +63,19 @@ public class ManagePage extends ContentPanel {
                 try {
                     selectedTable = newValue;
                     response = DBUtils.request(App.getConnection(), "select * from " + newValue, newValue);
-                    changeTableView(centerPane, tableView, response.getAsTableView());
+                    changeTableView(gridPane, tableView, response.getAsTableView());
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
             });
             response = DBUtils.request(App.getConnection(), "select * from " + tableSelector.getItems().get(0), tableSelector.getItems().get(0));
             System.out.println(response.getErrorMessage());
-            changeTableView(centerPane, tableView, response.getAsTableView());
+            changeTableView(gridPane, tableView, response.getAsTableView());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        centerPane.add(tableSelector, 0, 0);
+        gridPane.add(tableSelector, 0, 0);
+        vbox.getChildren().add(gridPane);
     }
 
     @Override
@@ -67,24 +86,44 @@ public class ManagePage extends ContentPanel {
     static void changeTableView(GridPane container, TableView<RowData> oldTable, TableView<RowData> newTable) throws SQLException {
         if(oldTable != null) container.getChildren().remove(oldTable);
         container.add(newTable, 0, 1);
-        GridPane columnNames = new GridPane();
-        container.add(columnNames, 0, 2);
-        int rowIndex = 0;
+
+        GridPane insertPane = new GridPane();
+        insertPane.setTranslateY(10);
+        container.add(insertPane, 0, 2);
+        int columnIndex = 0;
         List<TextArea> textAreas = new ArrayList<>();
         for (String column : DBUtils.getColumnNames(App.getConnection(), response.getTable(), true)) {
             TextArea textArea = new TextArea();
             textAreas.add(textArea);
             textArea.setPromptText(column);
-            columnNames.add(textArea, rowIndex, 2);
-            rowIndex++;
+            textArea.setMaxWidth(100);
+            textArea.setMaxHeight(50);
+            insertPane.add(textArea, columnIndex, 2);
+            columnIndex++;
         }
         Button button = getAddButton(container, newTable, textAreas);
-        columnNames.add(button, rowIndex, 2);
+        insertPane.add(button, columnIndex, 2);
+
+        GridPane deletePane = new GridPane();
+        deletePane.setTranslateY(26);
+        container.add(deletePane, 1, 1);
+        int rowIndex = 0;
+        Map<Button, RowData> rowButtons = new HashMap<>();
+        for (RowData row : newTable.getItems()){
+            Button deleteButton = new Button("Supprimer");
+            deleteButton.setMinWidth(100);
+            deleteButton.setMinHeight(24);
+            deleteButton.setMaxHeight(24);
+            rowButtons.put(deleteButton, row);
+            deletePane.add(deleteButton, 2, rowIndex);
+            rowIndex++;
+        }
     }
 
     private static Button getAddButton(GridPane container, TableView<RowData> newTable, List<TextArea> textAreas) {
         Button button = new Button("Ajouter");
         button.setMinWidth(100);
+        button.setMaxHeight(50);
         button.setOnAction(event -> {
             for (TextArea textArea : textAreas) {
                 System.out.println(textArea.getText());
